3b4b40af897f368a4e5c944da101d5ce6442575c
flashh-g <flashgordonn@protonmail.com>
2022-09-24 23:26:16 -0500

    added colored output and removed warnings

--- src/web_serv.rs
+++ src/web_serv.rs
@@ -1,6 +1,7 @@
 use crate::args::Arg;
 use crate::config::Config;
 use clap::Parser;
+use colored::Colorize;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -10,14 +11,16 @@ use std::{
 async fn process_socket_and_fmt_html(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+
     let status_line = if request_line == "GET / HTTP/1.1" {
         "HTTP/1.1 200 OK"
     } else {
         "HTTP?1.1 404 NOT FOUND"
     };
-    let arg = Arg::parse();
 
-    let content = fs::read_to_string(arg.entry_point).unwrap();
+    let arg = Arg::parse(); // parse command line arg
+
+    let content = fs::read_to_string(arg.entry_point).unwrap(); //entry point = index.html or something
     let length = content.len();
     let response = format!("{status_line}\r\n{length}\r\n\r\n{content}");
     stream.write_all(response.as_bytes()).unwrap();
@@ -26,12 +29,22 @@ pub async fn bind_to_ip_addr() {
     let config = Config::set_config(); //accessing config.toml feilds
     let ip = config.ip; // config.toml user set ip value
     let port = config.port.to_string(); // config.toml user set port value
-    let result = ip.to_string() + &":".to_string() + &port;
-    let listener = TcpListener::bind(result).unwrap();
-    println!("Listening on {:?}", &port);
+    let result = ip.to_string() + &":".to_string() + &port.as_ref();
+    let listener = TcpListener::bind(&result).unwrap();
+    let listen_on = "Listening on";
+
+    print!("\n\n");
+    println!(
+        "{}\n\n{}",
+        listen_on.bright_yellow().bold(),
+        result.to_string().bright_green()
+    );
+
     loop {
+        // event_loop
         let (stream, _) = listener.accept().unwrap();
         tokio::spawn(async move {
+            //spawn thread
             process_socket_and_fmt_html(stream).await;
         });
     }
